5e098a759296e43cb2eb886f10b97e2191562714
Ashley Nicholson <an00047@my.westga.edu>
2022-01-16 17:35:52 -0500

    Reformat text boxes and labels to utilize tableLayoutPanel and add design properties

--- TechSupport/view/LoginForm.Designer.cs
+++ TechSupport/view/LoginForm.Designer.cs
@@ -29,47 +29,66 @@ namespace TechSupport
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.passwordTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.loginButton = new System.Windows.Forms.Button();
             this.usernameLabel = new System.Windows.Forms.Label();
             this.passwordLabel = new System.Windows.Forms.Label();
             this.errorLabel = new System.Windows.Forms.Label();
             this.errorTextBox = new System.Windows.Forms.TextBox();
-            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // tableLayoutPanel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(200, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Log In";
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.19492F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.80508F));
+            this.tableLayoutPanel1.Controls.Add(this.usernameLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.loginButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.usernameTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.passwordTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.errorLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.passwordLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.errorTextBox, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.28426F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.71574F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(472, 233);
+            this.tableLayoutPanel1.TabIndex = 8;
             // 
             // usernameTextBox
             // 
+            this.usernameTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.usernameTextBox.Location = new System.Drawing.Point(206, 102);
+            this.usernameTextBox.Location = new System.Drawing.Point(188, 13);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(200, 30);
+            this.usernameTextBox.Size = new System.Drawing.Size(266, 30);
             this.usernameTextBox.TabIndex = 1;
             // 
-            // passwordTextBox
+            // loginButton
             // 
-            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.passwordTextBox.Location = new System.Drawing.Point(206, 163);
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(200, 30);
-            this.passwordTextBox.TabIndex = 2;
+            this.loginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loginButton.AutoSize = true;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(259, 177);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(138, 37);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Log In";
+            this.loginButton.UseVisualStyleBackColor = true;
             // 
             // usernameLabel
             // 
+            this.usernameLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.usernameLabel.AutoSize = true;
             this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.usernameLabel.Location = new System.Drawing.Point(98, 105);
+            this.usernameLabel.Location = new System.Drawing.Point(80, 15);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(102, 25);
             this.usernameLabel.TabIndex = 3;
@@ -77,9 +96,10 @@ namespace TechSupport
             // 
             // passwordLabel
             // 
+            this.passwordLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.passwordLabel.AutoSize = true;
             this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.passwordLabel.Location = new System.Drawing.Point(98, 166);
+            this.passwordLabel.Location = new System.Drawing.Point(84, 69);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(98, 25);
             this.passwordLabel.TabIndex = 4;
@@ -87,9 +107,10 @@ namespace TechSupport
             // 
             // errorLabel
             // 
+            this.errorLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.errorLabel.AutoSize = true;
             this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.errorLabel.Location = new System.Drawing.Point(60, 221);
+            this.errorLabel.Location = new System.Drawing.Point(42, 120);
             this.errorLabel.Name = "errorLabel";
             this.errorLabel.Size = new System.Drawing.Size(140, 25);
             this.errorLabel.TabIndex = 5;
@@ -97,54 +118,50 @@ namespace TechSupport
             // 
             // errorTextBox
             // 
+            this.errorTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.errorTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.errorTextBox.Location = new System.Drawing.Point(206, 221);
+            this.errorTextBox.Location = new System.Drawing.Point(188, 117);
             this.errorTextBox.Name = "errorTextBox";
-            this.errorTextBox.Size = new System.Drawing.Size(200, 30);
+            this.errorTextBox.Size = new System.Drawing.Size(266, 30);
             this.errorTextBox.TabIndex = 6;
             // 
-            // loginButton
+            // passwordTextBox
             // 
-            this.loginButton.AutoSize = true;
-            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(206, 278);
-            this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(100, 40);
-            this.loginButton.TabIndex = 7;
-            this.loginButton.Text = "Log In";
-            this.loginButton.UseVisualStyleBackColor = true;
+            this.passwordTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.passwordTextBox.Location = new System.Drawing.Point(188, 66);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(266, 30);
+            this.passwordTextBox.TabIndex = 2;
             // 
             // loginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(496, 341);
-            this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.errorTextBox);
-            this.Controls.Add(this.errorLabel);
-            this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.usernameLabel);
-            this.Controls.Add(this.passwordTextBox);
-            this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(496, 254);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox usernameTextBox;
-        private System.Windows.Forms.MaskedTextBox passwordTextBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label usernameLabel;
-        private System.Windows.Forms.Label passwordLabel;
-        private System.Windows.Forms.Label errorLabel;
-        private System.Windows.Forms.TextBox errorTextBox;
         private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox errorTextBox;
+        private System.Windows.Forms.MaskedTextBox passwordTextBox;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label passwordLabel;
     }
 }
 
